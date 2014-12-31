de79f2cae46e19568d391b08242821c4c31ef17d
Alford <wgalford@gmail.com>
2014-12-30 21:57:22 -0500

    calculator

---
+++ calculator.rb
@@ -0,0 +1,29 @@
+#My Calculator 
+require 'pry'
+
+def say(msg)
+  puts "_-_-_-_-_-_-_-_-_-#{msg}_-_-_-_-_-_-_-_-_-"
+end
+
+say "What's the first number"
+num1 = gets.chomp
+
+say "What is the second number"
+num2 = gets.chomp
+
+say "1) add 2) subtract 3) multiply 4) divide"
+operator = gets.chomp
+
+if operator == '1'
+  result = num1.to_i + num2.to_i
+elsif operator == '2'
+  result = num1.to_i - num2.to_i
+elsif operator == '3'
+  result = num1.to_i * num2.to_i
+elsif operator == '4'
+  result = num1.to_f / num2.to_f
+end
+
+puts "Result is #{result}"
+    
+    
